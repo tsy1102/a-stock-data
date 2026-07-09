05b8b85cc38391e7a7f8f332b606ce96b10f5b9a
User <user@example.com>
2026-07-09 22:48:05 +0800

    Clean up temporary diagnostic scripts and log files

--- _ful_with_timer.py
+++
@@ -1,5 +0,0 @@
-import faulthandler, os
-faulthandler.dump_traceback_later(50, exit=False)
-os.environ["PYTHONIOENCODING"] = "utf-8"
-import get_ful_report as g
-g.analyze_stock("000100", parallel=True)
